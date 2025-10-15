91204806f6cbe6ef4eabe0980e88d8fdda7d655d
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-15 19:56:38 +0200

    added. HomePage component

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import About from "./Components/About"
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+import Platforms from "./Components/Platforms"
+import Projects from "./Components/Projects"
+import ContactFooter from "./Components/ContactFooter"
+
+export default function HomePage(){
+    return(
+        <h2>sono home</h2>
+    )
+}
