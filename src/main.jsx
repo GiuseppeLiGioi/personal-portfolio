7d5dbc1ee9ff0f36b8c58961a2e096c724623e05
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-16 14:28:09 +0200

    fixed. import style in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
+import './styles/media-query-md.css'
+import './styles/media-query-lg.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
