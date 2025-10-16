9610d40255bf38562a9563c12b52524b5ae6ac52
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-16 12:40:02 +0200

    added. strcture of navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,13 @@
 export default function Navbar(){
     return(
-        <h2>sono Navbar</h2>
+        <nav className="container-navbar">
+            <ul className="list-link-navbar"> 
+                <li className="link-navbar"><a href="#hero">Home</a></li>
+                <li className="link-navbar"><a href="#about">About Me</a></li>
+                <li className="link-navbar"><a href="#projects">Progetti</a></li>
+                <li className="link-navbar"><a href="#platforms">Piattaforme</a></li>
+                <li className="link-navbar"><a href="#contact">Contatti</a></li>
+            </ul>
+        </nav>
     )
 }
