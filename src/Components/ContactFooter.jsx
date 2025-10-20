5659eb67f823fb3664398d5665d50067354352e2
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-20 18:47:33 +0200

    added. responsive style at ContactFooter

--- src/Components/ContactFooter.jsx
+++ src/Components/ContactFooter.jsx
@@ -5,7 +5,7 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 export default function ContactFooter() {
     return (
         <div className="container-footer">
-            <h2 className="title-about">RECAPITI E PIATTAFORME</h2>
+            <h2 className="title-contact">RECAPITI E PIATTAFORME</h2>
             <p className="description-footer">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Non perferendis odio dolorem consequatur molestiae error dicta itaque nisi, repellendus harum possimus sequi saepe. Modi ea iure optio soluta, animi dolor.
             </p>
@@ -13,17 +13,17 @@ export default function ContactFooter() {
             <div className="container-contacts">
 
                 <div className="container-info">
-                    <p><FontAwesomeIcon icon={faHouse} /> Via del cisto 42, Olbia, Sardegna, Italia</p>
-                    <a href="tel:+393403231407"><FontAwesomeIcon icon={faPhone} /><span>+39 3403231407</span></a>
-                    <a href="mailto:giuseppe.li.gioi.job@gmail.com"><FontAwesomeIcon icon={faEnvelope} /><span>giuseppe.li.gioi.job@gmail.com</span></a>
+                    <p className="p-contact p-contact-blue"><FontAwesomeIcon icon={faHouse} /> Via del cisto 42, Olbia, Sardegna, Italia</p>
+                    <a href="tel:+393403231407"><FontAwesomeIcon icon={faPhone} /><span className="span-contact">+39 3403231407</span></a>
+                    <a href="mailto:giuseppe.li.gioi.job@gmail.com"><FontAwesomeIcon icon={faEnvelope} /><span className="span-contact">giuseppe.li.gioi.job@gmail.com</span></a>
                 </div>
 
                 <div className="container-platforms">
-                    <a href="https://github.com/GiuseppeLiGioi" target="_blank" rel="noopener noreferrer">
+                    <a href="https://github.com/GiuseppeLiGioi" target="_blank" rel="noopener noreferrer" className="a-contact">
                         <FontAwesomeIcon icon={faGithub} /> GitHub
                     </a>
 
-                    <a href="https://www.linkedin.com/in/giuseppe-li-gioi-327b78378" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.linkedin.com/in/giuseppe-li-gioi-327b78378" target="_blank" rel="noopener noreferrer" className="a-contact">
                         <FontAwesomeIcon icon={faLinkedin} /> Linkedin
                     </a>
 
