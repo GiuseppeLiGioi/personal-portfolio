1b7831e45e1313f62c5414fdde00e7e81a03d602
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-22 13:57:12 +0200

    fixed. hero css

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown} from "@fortawesome/free-solid-svg-icons";
+
 export default function Hero(){
     return(
         <section id="hero">
@@ -20,12 +23,12 @@ export default function Hero(){
             <div className="hero-divider"></div>
 
             <div className="container-hero-info">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Voluptates nihil nemo quod facere quasi repellat nostrum dolore sint quam? Quidem aspernatur libero dolore pariatur ipsum quos enim tempora! Laboriosam, eum!
+                <p>Ciao! Sono Giuseppe Li Gioi, ho vent'anni e sono uno sviluppatore web Jr. full-stack. Adoro concretizzare le mie <strong>IDE</strong>e attraverso il codice, creando siti web ed interfacce utente.<br />
+                   Ogni giorno cerco di migliorarmi, imparando dai miei errori e sperimentando nuove tecnologie: questa è, secondo me, la chiave per crescere costantemente come sviluppatore.  
                 </p>
 
                 <a className="btn-hero" href="#projects">
-                   Scopri i miei Progetti
+                  Scopri i miei Progetti <FontAwesomeIcon  icon={faArrowDown}/> 
                 </a>
             </div>
         </div>
