9e976dda3c91a03bd4c09941bd5b035d87a53b24
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-24 17:42:32 +0200

    changed. name of navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,8 +3,8 @@ export default function Navbar(){
         <nav className="container-navbar fade-in-up">
             <div className="navbar-inner">
             <ul className="list-link-navbar"> 
-                <li className="link-navbar"><a href="#hero">Home</a></li>
-                <li className="link-navbar"><a href="#about">About Me</a></li>
+                <li className="link-navbar"><a href="#hero">Intestazione</a></li>
+                <li className="link-navbar"><a href="#about">Profilo</a></li>
                 <li className="link-navbar"><a href="#projects">Progetti</a></li>
                 <li className="link-navbar"><a href="#contact">Contatti</a></li>
             </ul>
